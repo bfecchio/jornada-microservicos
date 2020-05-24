a51ece1e0a02872185c8e5b40021ca6fbfab01bb
bfecchio <bruno.fecchio@gmail.com>
2020-05-24 13:53:39 -0300

    Inclusao do serviço de customer

---
+++ customer_service/app/Models/OrderItem.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use Uuid;
+    public $increment = false;
+    protected $keyType = "string";
+
+    protected $fillable = [
+        "id",
+        "order_id",
+        "product_id",
+        "qtd",
+        "total",
+    ];
+
+    protected $casts = [
+        'qtd' => 'int',
+        'total' => 'float'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+}
